8ca55ed7b3bba0adb23da5b29a191c7006d3a7a0
RicardoOSantos <99694981+RicardoOSantos@users.noreply.github.com>
2022-03-26 23:03:40 -0300

    Add files via upload

---
+++ ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    public class Elevador
+    {
+        protected int andarAtual;
+        protected int totalAndares;
+        protected int capacidade;
+        protected int quantiaPessoas;
+
+        public void Inicializar(int capacidadeD, int totalAndaresD)
+        { //inicia o elevador dando o total de andares, capacidade, andar terreo e 0 pessoas
+            andarAtual = 0;
+            quantiaPessoas = 0;
+            capacidade = capacidadeD;
+            totalAndares = totalAndaresD;
+        }
+
+        public string Mostrar()
+        {
+            return $"O elevador está no {andarAtual} andar, tem {capacidade} de capacidade e tem {quantiaPessoas} pessoas";
+        }
+
+        public int Entrar()
+        {
+            if (quantiaPessoas < capacidade)
+            {
+                Console.WriteLine($"Tinham {quantiaPessoas} no elevador, entrou 1 pessoa, agora temos {quantiaPessoas + 1} pessoas");
+                quantiaPessoas = quantiaPessoas + 1;
+            }
+            else
+            {
+                Console.WriteLine($"O elevador está cheio, a capacidade de {capacidade} foi atingida");
+            }
+
+            return quantiaPessoas;
+        }
+
+        public int Sair()
+        {
+            if (quantiaPessoas > 0)
+            {
+                Console.WriteLine($"Tinham {quantiaPessoas} no elevador, saiu 1, agora temos {quantiaPessoas - 1} pessoas");
+                quantiaPessoas = quantiaPessoas - 1;
+            }
+            else
+            {
+                Console.WriteLine("O elevador está vazio");
+            }
+
+            return quantiaPessoas;
+        }
+
+        public int Subir()
+        {
+            Console.WriteLine("Para qual andar você quer ir?");
+            int andarIr = int.Parse(Console.ReadLine());
+
+            if (andarIr < andarAtual)
+            {
+                Console.WriteLine("Não dá para subir para baixo!");
+
+            }
+            else if (andarIr <= totalAndares)
+            {
+
+                Console.WriteLine($"Estavamos no {andarAtual} andar, subimos e agora estamos no {andarIr} andar");
+                andarAtual = andarIr;
+            }
+            else
+            {
+
+                Console.WriteLine("Não é possível ir para esse andar!");
+            }
+
+            return andarAtual;
+        }
+
+        public int Descer()
+        {
+            Console.WriteLine("Para qual andar você quer ir?");
+            int andarIr = int.Parse(Console.ReadLine());
+
+            if (andarIr > andarAtual)
+            {
+                Console.WriteLine("Não dá para descer para cima!");
+
+            }
+            else if (andarIr <= totalAndares && andarIr >= 0)
+            {
+
+                Console.WriteLine($"Estavamos no {andarAtual} andar, descemos e agora estamos no {andarIr} andar");
+                andarAtual = andarIr;
+            }
+            else
+            {
+
+                Console.WriteLine("Não é possível ir para esse andar!");
+            }
+
+            return andarAtual;
+        }
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,83 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuarB = false;
+
+
+            //do
+            //{           
+
+            Console.WriteLine("qual a capacidade do elevador?");
+            int capacidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("quantos andares tem o elevador?");
+            int totalAndares = int.Parse(Console.ReadLine());
+
+            Elevador el = new Elevador();
+            el.Inicializar(capacidade, totalAndares);
+
+            do
+            {
+                Console.WriteLine($@" O que você deseja fazer?
+                        E - Entrar uma pessoa
+                        S - Sair uma pessoa
+                        Su - Subir
+                        D - Descer");
+
+                string opcao = Console.ReadLine().ToUpper();
+
+                switch (opcao)
+                {
+                    case "E":
+                        el.Entrar();
+                        break;
+
+                    case "S":
+                        el.Sair();
+                        break;
+
+                    case "SU":
+                        el.Subir();
+                        break;
+
+                    case "D":
+                        el.Descer();
+                        break;
+
+                    default:
+                        Console.WriteLine("Essa opcao é invalida, digite uma valida");
+                        break;
+                }
+
+                Console.WriteLine("Deseja continuar? s para sim");
+                string continuar = Console.ReadLine().ToLower();
+
+                if (continuar == "s")
+                {
+                    continuarB = true;
+                }
+                else
+                {
+                    continuarB = false;
+                }
+
+            } while (continuarB == true);
+
+            // break;
+
+
+
+            // } while (continuarB == false);
+
+
+
+        }
+    }
+    
+}
